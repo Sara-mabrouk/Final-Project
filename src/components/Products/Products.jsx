d309b9296aea9a1bc121072cce65abbbf3805d97
Your Name <you@example.com>
2024-09-09 20:20:53 +0300

    update WishList-

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -53,6 +53,9 @@ export default function Products({ product }) {
                         </div>
                     </div>
                 </div>
-            </div></>
+                
+            </div>
+            
+            </>
     )
 }
